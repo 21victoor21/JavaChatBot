9eefa09e15c5fdd8417a2c68477a5318232fe613
unknown <21victoor21@gmail.com>
2024-06-27 06:43:13 -0500

    Finished Project

--- TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     public static final String NAME = "RootCodeGym_bot";
@@ -18,16 +22,73 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
         if (getMessageText().equals("/start")){
-            sendTextMessageAsync("Hola, _futuro_ *programador* Victor");
+            setUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
+            sendTextMessageAsync(STEP_1_TEXT,
+                    Map.of("hackear la nevera","step_1_btn"));
         }
 
-        if (getMessageText().contains("hola")){
-            sendTextMessageAsync("Hola, cual es tu nombre?");
+        if (getCallbackQueryButtonKey().equals("step_1_btn")){
+            setUserGlory(20);
+            sendPhotoMessageAsync("step_2_pic");
+            sendTextMessageAsync(STEP_2_TEXT,
+                    Map.of("!Tomar una salchicha¡ +20 defama","step_2_btn",
+                    "!Tomar una pescado¡ +20 defama","step_2_btn",
+                    "!Tirar una lata de pepinillos¡ +20 defama","step_2_btn"));
         }
 
-        if (getMessageText().contains("me llamo")){
-            sendTextMessageAsync("Encantado de conocerte, soy *Gato*");
+        if (getCallbackQueryButtonKey().equals("step_2_btn")){
+            setUserGlory(20);
+            sendPhotoMessageAsync("step_3_pic");
+            sendTextMessageAsync(STEP_3_TEXT,
+                    Map.of("Hackear al robot aspirador","step_3_btn"));
         }
+
+        if (getCallbackQueryButtonKey().equals("step_3_btn")){
+            setUserGlory(30);
+            sendPhotoMessageAsync("step_4_pic");
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of("!Enviar al robot aspirador por comida¡ +30 de fama","step_4_btn",
+                            "!Dar un paseo en el robot espirador¡ +30 de fama","step_4_btn",
+                            "!Huir del robot aspirador¡ +30 de fama","step_4_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_4_btn")){
+            setUserGlory(30);
+            sendPhotoMessageAsync("step_5_pic");
+            sendTextMessageAsync(STEP_5_TEXT,
+                    Map.of("Encender y ponerse la GoPro","step_5_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_5_btn")){
+            setUserGlory(40);
+            sendPhotoMessageAsync("step_6_pic");
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of("!Correr por los tejados, grabar con la GoPro¡ +40 de fama","step_6_btn",
+                            "!Atacar a otros gatos desde tu escondite con la GoPro¡ +40 de fama","step_6_btn",
+                            "!Atacar a los perros desde tu escondite con la GoPro¡ +40 de fama","step_6_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_6_btn")){
+            addUserGlory(40);
+            sendPhotoMessageAsync("step_7_pic");
+            sendTextMessageAsync(STEP_7_TEXT,
+                    Map.of("Romper la contraseña","step_7_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_7_btn")){
+            addUserGlory(50);
+            sendPhotoMessageAsync("step_8_pic");
+            sendTextMessageAsync(STEP_8_TEXT,
+                    Map.of("Salir al patio","step_8_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_8_btn")){
+            addUserGlory(50);
+            sendPhotoMessageAsync("final_pic");
+            sendTextMessageAsync(FINAL_TEXT);
+        }
+
     }
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
